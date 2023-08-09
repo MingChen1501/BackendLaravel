02666477bf6a07fd5c00c83cec90c4df54ab8965
MinhTran <tminh400@gmail.com>
2023-08-09 17:29:16 +0700

    create migration for new model
    * create migration for 4 new tables : texts, text_configs, audios, touchable_objects
    
    * disable foreign key while down()

---
+++ database/migrations/2023_08_09_101551_create_touchable_objects_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('touchable_objects', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('page_id')->constrained('pages');
+            $table->foreignId('text_id')->constrained('texts');
+            $table->json('position');
+            $table->integer('order');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::disableForeignKeyConstraints();
+        Schema::dropIfExists('touchable_objects');
+    }
+};
